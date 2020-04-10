cd7d99e5973dbd1233108e963ecc38b74076bf2b
Konstantin Belousov <bks79@icloud.com>
2020-04-10 15:37:40 +0300

    retrofit

---
+++ app/src/main/java/com/Illarionov/art/rest_api/ArtistApiService.kt
@@ -0,0 +1,90 @@
+package com.Illarionov.art.rest_api
+
+import io.reactivex.Observable
+import io.reactivex.Single
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ArtistApiService {
+
+//    companion object {
+//
+//        fun create(): ArtistApiService {
+//            val loggingInterceptor = HttpLoggingInterceptor()
+//            loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+//
+//            val client = OkHttpClient.Builder()
+//                    .addInterceptor(loggingInterceptor)
+//                    .build()
+//
+//            val retrofit = Retrofit.Builder()
+//                    .baseUrl(BuildConfig.BASE_URL)
+//                    .client(client)
+//                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+//                    .addConverterFactory(ScalarsConverterFactory.create())
+//                    .addConverterFactory(GsonConverterFactory.create())
+//                    .build()
+//
+//            return retrofit.create(ArtistApiService::class.java)
+//        }
+//    }
+//
+//    @GET("artists.info")
+//    fun getArtistInfo(@Query("artist_ids") artistId: Int = BuildConfig.ARTIST_ID,
+//                      @Query("extends") extends: String = "artists.hello,artists.about_app,artists.media_id,artists.count_works,artists.biography,artists.properties,artists.about_app,artists.contact")
+//            : Single<ArtistsResponce>
+//
+//    @GET("expositions.search")
+//    fun getExpositions(@Query("date_start_to") startDate: Long,
+//                       @Query("date_end_from") endDate: Long,
+//                       @Query("uri_owner") uri_owner: String = "artist://${BuildConfig.ARTIST_ID}",
+//                       @Query("offset") offset: Int = 0,
+//                       @Query("count") count: Int = 20,
+//                       @Query("extends") extends: String = "expositions.media_ids")
+//    : Single<ExpositionsResponse>
+//
+//    @GET("events.artist")
+//    fun getArtistNews(@Query("before_event_id") beforeEventId: Long = 0L,
+//                      @Query("count") count: Int = 20,
+//                      @Query("artist_id") artistId: Int = BuildConfig.ARTIST_ID,
+//                      @Query("extends") extends: String = "events.attaches,works.description,works.properties,expositions.media_ids,set.info,artists.hello,works.media_id,works.counters,topics.first_media_id,topics.full,set.info,artists.hello,topics.uris")
+//    : Observable<EventsResponse>
+//
+//    @GET("sets.artist_sets")
+//    fun getCollections(@Query("type") type: String = "artist",
+//                       @Query("order") order: String = "date",
+//                       @Query("direction") direction: String = "desc",
+//                       @Query("count") count: Int = 10,
+//                       @Query("offset") offset: Int = 0,
+//                       @Query("artist_id") artistId: Int = BuildConfig.ARTIST_ID,
+//                       @Query("extends") extends: String = "asets.cover_work_id")
+//            : Observable<CollectionResponce>
+//
+//    @GET("works.search")
+//    fun getWorks(@Query("count") count: Int = 10,
+//                 @Query("offset") offset: Int = 0,
+//                 @Query("order") order: String = "",
+//                 @Query("artist_id") artistId: Int = BuildConfig.ARTIST_ID,
+//                 @Query("uris") uris: String = "",
+//                 @Query("min_x") min_x: String = "",
+//                 @Query("max_x") max_x: String = "",
+//                 @Query("extends") extends: String = "works.media_id,works.counters,works.properties,works.collection_id,works.infos,works.description,filters.uri,works.aset_ids,works.artist_ids,works.media_id")
+//            : Observable<WorksResponse>
+//
+//    @GET("works.search")
+//    fun getFilters(@Query("with_filters") with_filters: String = "y",
+//                   @Query("filter_prefixes") filter_prefixes: String = "salest,genre,style,tech",
+//                   @Query("count_filters") count_filters: Int = 20,
+//                   @Query("count") count: Int = 0)
+//            : Observable<FiltersResponse>
+//
+//    @GET("static.info")
+//    fun getInfoForUsers(@Query("section") section: String)
+//            : Observable<InfoResponse>
+}
