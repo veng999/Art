87a31cda8a96a7e45bb7e813988130e3329f7aa1
user <vitaliy_illarionov@senla.eu>
2020-04-27 15:26:43 +0300

    Added colors to the placeholder, rendered the logic of working with ImageView into the extension function, and changed the parent of the datasource to PageKeyedDataSource

--- app/src/main/java/com/Illarionov/art/network/WorksRemoteDataSource.kt
+++ app/src/main/java/com/Illarionov/art/network/WorksRemoteDataSource.kt
@@ -2,6 +2,7 @@ package com.Illarionov.art.network
 
 import android.util.Log
 import androidx.paging.ItemKeyedDataSource
+import androidx.paging.PageKeyedDataSource
 import com.Illarionov.art.rest_api.ArtistApiService
 import com.company.myartist.model.Work
 import com.company.myartist.model.response.WorksResponse
@@ -9,14 +10,17 @@ import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.rxkotlin.addTo
 import io.reactivex.schedulers.Schedulers
 
-class WorksRemoteDataSource : ItemKeyedDataSource<Long, Work>() {
+class WorksRemoteDataSource : PageKeyedDataSource<Long, Work>() {
 
     private val api: ArtistApiService = ArtistApiService.create()
     private val compositeDisposable = CompositeDisposable()
     private val TAG = WorksRemoteDataSource::class.java.simpleName
 
     //todo список не подгружается по странично проверь запросы в логе и тот ли ты DataSource выбрал?
-    override fun loadInitial(params: LoadInitialParams<Long>, callback: LoadInitialCallback<Work>) {
+    override fun loadInitial(
+        params: LoadInitialParams<Long>,
+        callback: LoadInitialCallback<Long, Work>
+    ) {
         api.getWorks()
             .subscribeOn(Schedulers.io())
             .observeOn(Schedulers.computation())
@@ -25,7 +29,7 @@ class WorksRemoteDataSource : ItemKeyedDataSource<Long, Work>() {
                 val works = getData(it)
                 val position = works.size
                 val totalCount = getTotalCount(it)
-                callback.onResult(works, position, totalCount)
+                callback.onResult(works, position.toLong(), totalCount.toLong())
             },
             {
                 Log.e(TAG, "Fetch events failed: ${it.localizedMessage}")
@@ -41,14 +45,14 @@ class WorksRemoteDataSource : ItemKeyedDataSource<Long, Work>() {
         return (response?.data?.count ?: "0").toInt()
     }
 
-    override fun loadAfter(params: LoadParams<Long>, callback: LoadCallback<Work>) {
+    override fun loadAfter(params: LoadParams<Long>, callback: LoadCallback<Long, Work>) {
         api.getWorks(params.key)
             .subscribeOn(Schedulers.io())
             .observeOn(Schedulers.computation())
             .subscribe(
                 {
                     val works = getData(it)
-                    callback.onResult(works)
+                    callback.onResult(works, params.key + 1)
                 },
                 {
                     Log.e(TAG, "Fetch events failed: ${it.localizedMessage}")
@@ -56,11 +60,7 @@ class WorksRemoteDataSource : ItemKeyedDataSource<Long, Work>() {
             ).addTo(compositeDisposable)
     }
 
-    override fun loadBefore(params: LoadParams<Long>, callback: LoadCallback<Work>) {
-    }
-
-    override fun getKey(item: Work): Long {
-        return item.work_id!!.toLong() // ты серьезно хочешь чтобы приложение упало если id null
+    override fun loadBefore(params: LoadParams<Long>, callback: LoadCallback<Long, Work>) {
     }
 
     fun clear() {
--- app/src/main/java/com/Illarionov/art/view/ui/works/WorksViewModel.kt
+++ app/src/main/java/com/Illarionov/art/view/ui/works/WorksViewModel.kt
@@ -1,22 +1,13 @@
 package com.Illarionov.art.view.ui.works
 
-import android.util.Log
-import android.view.MenuItem
 import androidx.lifecycle.ViewModel
 import androidx.paging.DataSource
 import androidx.paging.LivePagedListBuilder
 import androidx.paging.PagedList
-import com.Illarionov.art.R
 import com.Illarionov.art.network.NewsRemoteDataSource
 import com.Illarionov.art.network.WorksRemoteDataSource
 import com.company.myartist.model.Work
-import com.google.android.material.bottomnavigation.BottomNavigationView
 
-//ViewModel ничего не должна знать о виджетах и прочих компонентах с которыми взаимодействует user у
-//него другая задача, он должен реализовать получение данных от слоя model и выполнить некую бизнеслогику
-//class WorksViewModel : ViewModel(), BottomNavigationView.OnNavigationItemSelectedListener {
-//todo разберись по внимательней с компонентом Bottom Navigation Menu и компонентом навигации в андроидx
-//пишешь лишний код
 class WorksViewModel : ViewModel() {
 
     val dataSource = NewsRemoteDataSource()
@@ -38,16 +29,6 @@ class WorksViewModel : ViewModel() {
         }
         return LivePagedListBuilder(dataSourceFactory, config)
     }
-
-//    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-//        return when (item.itemId) {
-//            R.id.menu_works -> {
-//                return true
-//            }
-//            else -> false
-//        }
-//    }
-
 
     override fun onCleared() {
         super.onCleared()
